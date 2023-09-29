171741030fe470f7b72968d631cf857602c21c9f
albatror <r3vzone@gmail.com>
2023-09-29 13:41:28 +0200

    Update build.sh
    fixed

--- build.sh
+++ build.sh
@@ -1,25 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
 # Execute the Python script to update offsets.h
 python3 updater.py
 
 # Check if the Python script was successful
 if [ $? -eq 0 ]; then
-    cd memflow_lib/memflow-win32-ffi/
-    
-    if cargo build --release ; then
-        cd ../memflow-qemu-procfs
-    
-        if cargo build --release --all-features ; then
-            cd ../../
-            make
-        else
-            echo "Error while building memflow-qemu-procfs"
-        fi
-    
+    path="apex_dma/build"
+    if [ -d "$path" ]; then
+        cd "$path" && ninja;
     else
-        echo "Error while building memflow-win32-ffi"
+        cd vmread && meson "../$path" && cd "../$path" && ninja;
     fi
 else
     echo "Error while running updater.py"
 fi
+

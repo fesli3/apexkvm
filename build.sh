241a4b56fafd86ac8e3758a1415bb7a1dea2a604
albatror <r3vzone@gmail.com>
2023-09-29 13:40:28 +0200

    Update build.sh
    addind updater.py to project and add it to build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,25 @@
-#!/bin/sh
-path="apex_dma/build"
-if [ -d "$path" ]; then
-	cd "$path" && ninja;
+#!/bin/bash
+
+# Execute the Python script to update offsets.h
+python3 updater.py
+
+# Check if the Python script was successful
+if [ $? -eq 0 ]; then
+    cd memflow_lib/memflow-win32-ffi/
+    
+    if cargo build --release ; then
+        cd ../memflow-qemu-procfs
+    
+        if cargo build --release --all-features ; then
+            cd ../../
+            make
+        else
+            echo "Error while building memflow-qemu-procfs"
+        fi
+    
+    else
+        echo "Error while building memflow-win32-ffi"
+    fi
 else
-	cd vmread && meson "../$path" && cd "../$path" && ninja;
+    echo "Error while running updater.py"
 fi

fbe058c96210abaf1c35e9c2674cdfe33bddb642
fesli3 <930864252@qq.com>
2025-03-31 16:41:20 +0800

    Add files via upload

--- apex_dma/offsets.h
+++ apex_dma/offsets.h
@@ -1,8 +1,8 @@
-#define GameVersion v3.0.2.31 //[Miscellaneous].GameVersion updated 2025/01/08
-#define OFFSET_ENTITYLIST 0x6a44a68 //[Miscellaneous].cl_entitylist updated 2025/03/07
-#define OFFSET_LOCAL_ENT 0x2684A38 //[Miscellaneous].LocalPlayer updated 2025/03/07
-#define OFFSET_LOCAL_ENTITY_HANDLE 0x1aa336c //[Miscellaneous].LocalEntityHandle updated 2025/03/07
-#define OFFSET_NAME_LIST 0x949eaa0 //[Miscellaneous].NameList updated 2025/03/07
+#define GameVersion v3.0.3.17 //[Miscellaneous].GameVersion updated 2025/03/25
+#define OFFSET_ENTITYLIST 0x6a44a68 //[Miscellaneous].cl_entitylist updated 2025/03/31
+#define OFFSET_LOCAL_ENT 0x2684a38 //[Miscellaneous].LocalPlayer updated 2025/03/31
+#define OFFSET_LOCAL_ENTITY_HANDLE 0x1aa336c //[Miscellaneous].LocalEntityHandle updated 2025/03/31
+#define OFFSET_NAME_LIST 0x95a6d90 //[Miscellaneous].NameList updated 2025/03/31
 #define OFFSET_GLOBAL_VARS 0x1b724d0 //[Miscellaneous].GlobalVars updated 2025/02/25
  
 #define OFFSET_LEVELNAME 0x1b729a4 //[Miscellaneous].LevelName updated 2025/02/25
@@ -10,30 +10,30 @@
 //#define OFFSET_CLIENTSTATE 0x1836670 //[Miscellaneous].ClientState updated 2024/10/09
 #define OFFSET_SIGNONSTATE OFFSET_CLIENTSTATE + 0xAC //SignonState (ClientState + 0xAC)  //updated 1/10/2024
 //#define OFFSET_SIGNONSTATE OFFSET_CLIENTSTATE 0x16d699c
-#define OFFSET_HOST_MAP 0x01a8f430 + 0x58 //[ConVars].host_map + 0x58 updated 2025/03/07
+#define OFFSET_HOST_MAP 0x01a8f430 + 0x58 //[ConVars].host_map + 0x58 updated 2025/03/31
  
 #define OFFSET_TEAM 0x0338 //[RecvTable.DT_BaseEntity].m_iTeamNum updated 2025/02/25
-#define OFFSET_HEALTH 0x0328 //[RecvTable.DT_Player].m_iHealth updated updated 2025/03/07
+#define OFFSET_HEALTH 0x0328 //[RecvTable.DT_Player].m_iHealth updated updated 2025/03/31
 #define OFFSET_MAXHEALTH 0x0470 //[RecvTable.DT_Player].m_iMaxHealth
 #define OFFSET_TRAVERSAL_RELEASE_TIME 0x2bd0 //[RecvTable.DT_LocalPlayerExclusive].m_traversalReleaseTime
 
 
 #define OFFSET_SHIELD 0x01a0 //[RecvTable.DT_BaseEntity].m_shieldHealth updated 2024/09/30
 #define OFFSET_MAXSHIELD 0x1A4 //[RecvTable.DT_BaseEntity].m_shieldHealthMax updated 2024/09/30
-#define OFFSET_ARMORTYPE 0x47e4 //[RecvTable.DT_Player].m_armorType updated updated 2025/03/07
+#define OFFSET_ARMORTYPE 0x47e4 //[RecvTable.DT_Player].m_armorType updated updated 2025/03/31
 #define OFFSET_NAME 0x0481 //[RecvTable.DT_BaseEntity].m_iName updated 2025/02/25
 #define OFFSET_SIGN_NAME 0x0478 //[RecvTable.DT_BaseEntity].m_iSignifierName updated 2025/02/25
 #define OFFSET_ABS_VELOCITY 0x0170 //[DataMap.C_BaseEntity].m_vecAbsVelocity updated 2024/09/18
-#define OFFSET_VISIBLE_TIME 0x19f0 //[Miscellaneous].CPlayer!lastVisibleTime updated 2025/03/07
-#define OFFSET_LAST_AIMEDAT_TIME 0x1a18   //[Miscellaneous].CPlayer!lastVisibleTime + 0x8
-#define OFFSET_ZOOMING 0x1c41 //[RecvTable.DT_Player].m_bZooming updated 2025/03/07
+#define OFFSET_VISIBLE_TIME 0x19f0 //[Miscellaneous].CPlayer!lastVisibleTime updated 2025/03/31
+#define OFFSET_LAST_AIMEDAT_TIME 0x19f0   //[Miscellaneous].CPlayer!lastVisibleTime + 0x8
+#define OFFSET_ZOOMING 0x1c41 //[RecvTable.DT_Player].m_bZooming updated 2025/03/31
 
 #define OFFSET_VIEW_OFFSET 0x00e8 //[DataMap.C_BaseEntity].m_currentFrame.viewOffset updated 2024/09/18
-#define OFFSET_ACTIVE_WEAPON 0x1928 + 0x0058 //[RecvTable.DT_Player].m_inventory + WeaponInventory_Client>activeWeapons updated 2025/03/07
+#define OFFSET_ACTIVE_WEAPON 0x1928 + 0x0058 //[RecvTable.DT_Player].m_inventory + WeaponInventory_Client>activeWeapons updated 2025/03/31
 
 #define OFFSET_SPECTATOR_LIST OFFSET_OBSERVER_LIST //? updated 2024/11/15
 //#define OFFSET_OBSERVER_LIST 0x01ff4ce0 + 0x20C8 //[ConVars].gamepad_aim_assist_melee + 0x20C8 updated 2025/01/08
-#define OFFSET_OBSERVER_LIST 0x6a46a88  //[ConVars].gamepad_aim_assist_melee + 0x20C8 updated 2025/03/07
+#define OFFSET_OBSERVER_LIST 0x0252b420  //[ConVars].gamepad_aim_assist_melee + 0x20C8 updated 2025/03/31
 #define OFFSET_OBSERVER_ARRAY 0x954 //[DataMapTypes.C_GlobalNonRewinding].m_playerObserver - [DataMap.C_ObserverMode].m_observerTarget updated 2024/09/20
 
 #define OFFSET_IN_DUCKSTATE 0x2ab8 //[DataMap.C_Player].m_duckState updated 2025/02/25
@@ -52,36 +52,36 @@
 #define OFFSET_FLAGS 0x00c8 //[DataMap.C_Player].m_fFlags updated 2025/02/25
 #define OFFSET_IN_ATTACK 0x03357b28 //[Buttons].in_attack updated 2025/03/07
 #define OFFSET_IN_ZOOM 0x03357cb0 //[Buttons].in_zoom updated 2025/03/07
-#define OFFSET_IN_FORWARD 0x03357d58 //[Buttons].in_forward updated 2025/03/07
-#define OFFSET_IN_BACKWARD 0x03357d80 //[Buttons].in_backward updated 2025/03/07
+#define OFFSET_IN_FORWARD 0x03357d58 //[Buttons].in_forward updated 2025/03/31
+#define OFFSET_IN_BACKWARD 0x03357d80 //[Buttons].in_backward updated 2025/03/31
  
-#define OFFSET_LIFE_STATE 0x0690 //[RecvTable.DT_Player].m_lifeState updated 2025/03/07
-#define OFFSET_BLEED_OUT_STATE 0x27b8 //[RecvTable.DT_Player].m_bleedoutState updated 2025/03/07
+#define OFFSET_LIFE_STATE 0x0690 //[RecvTable.DT_Player].m_lifeState updated 2025/03/31
+#define OFFSET_BLEED_OUT_STATE 0x27b8 //[RecvTable.DT_Player].m_bleedoutState updated 2025/03/31
  
 #define OFFSET_ORIGIN 0x017c //[DataMap.C_BaseEntity].m_vecAbsOrigin updated 2025/01/08
-#define OFFSET_BONES 0x0d88 + 0x48 //[RecvTable.DT_BaseAnimating].m_nForceBone + 0x48 updated 2025/03/07
-#define OFFSET_STUDIOHDR 0xfd0 //[Miscellaneous].CBaseAnimating!m_pStudioHdr updated 2025/03/07
+#define OFFSET_BONES 0x0d88 + 0x48 //[RecvTable.DT_BaseAnimating].m_nForceBone + 0x48 updated 2025/03/31
+#define OFFSET_STUDIOHDR 0xfd0 //[Miscellaneous].CBaseAnimating!m_pStudioHdr updated 2025/03/31
 #define OFFSET_AIMPUNCH 0x2438 //[DataMap.C_Player].m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle updated 2024/09/18
-#define OFFSET_CAMERAPOS 0x1f40 //[Miscellaneous].CPlayer!camera_origin updated 2025/03/07
-#define OFFSET_VIEWANGLES 0x2594 - 0x14 //[RecvTable.DT_Player].m_ammoPoolCapacity - 0x14 updated 2025/03/07
+#define OFFSET_CAMERAPOS 0x1f40 //[Miscellaneous].CPlayer!camera_origin updated 2025/03/31
+#define OFFSET_VIEWANGLES 0x2594 - 0x14 //[RecvTable.DT_Player].m_ammoPoolCapacity - 0x14 updated 2025/03/31
 #define OFFSET_BREATH_ANGLES OFFSET_VIEWANGLES - 0x10 //updated 01/10/2024
 #define OFFSET_OBSERVER_MODE 0x35e4 //[RecvTable.DT_LocalPlayerExclusive].m_iObserverMode updated 2025/02/25
 #define OFFSET_OBSERVING_TARGET 0x35f0 //[RecvTable.DT_LocalPlayerExclusive].m_hObserverTarget updated 2025/02/25
 #define OFFSET_IN_USE 0x03357c90 //[Buttons].in_use updated 2025/02/25
  
-#define OFFSET_MATRIX 0x11a350 //[Miscellaneous].ViewMatrix updated 2025/03/07
-#define OFFSET_RENDER 0x3355258 //[Miscellaneous].ViewRender updated 2025/03/07
+#define OFFSET_MATRIX 0x11a350 //[Miscellaneous].ViewMatrix updated 2025/03/31
+#define OFFSET_RENDER 0x3355258 //[Miscellaneous].ViewRender updated 2025/03/31
  
-#define OFFSET_WEAPON 0x1994 //[RecvTable.DT_BaseCombatCharacter].m_latestPrimaryWeapons updated 2025/03/07
+#define OFFSET_WEAPON 0x1994 //[RecvTable.DT_BaseCombatCharacter].m_latestPrimaryWeapons updated 2025/03/31
 #define OFFSET_BULLET_SPEED 0x2780 //0x19d8 + 0x04ec //0x1eb4 + 0x19d0 //[Miscellaneous].CWeaponX!m_flProjectileSpeed updated 2025/02/12
 #define OFFSET_BULLET_SCALE 0x2788 //0x19d8 + 0x04f4 //0x1ebc + 0x19d0 //[Miscellaneous].CWeaponX!m_flProjectileScale updated 2025/02/12
 #define OFFSET_ZOOM_FOV 0x16ec //[RecvTable.DT_WeaponX].m_playerData + m_curZoomFOV updated 2025/02/25
-#define OFFSET_AMMO 0x15b0 //[RecvTable.DT_PropSurvival].m_ammoInClip updated 2025/03/07
+#define OFFSET_AMMO 0x15b0 //[RecvTable.DT_PropSurvival].m_ammoInClip updated 2025/03/31
  
 //#define OFFSET_ITEM_GLOW 0x02f0 //m_highlightFunctionBits
-#define OFFSET_ITEM_ID 0x15b4 //[RecvTable.DT_PropSurvival].m_customScriptInt updated 2025/03/07
+#define OFFSET_ITEM_ID 0x15b4 //[RecvTable.DT_PropSurvival].m_customScriptInt updated 2025/03/31
 #define OFFSET_MODELNAME 0x0030 //[DataMap.C_BaseEntity].m_ModelName updated 2024/09/18
-#define OFFSET_M_CUSTOMSCRIPTINT OFFSET_ITEM_ID //[RecvTable.DT_PropSurvival].m_customScriptInt updated 2025/03/07
+#define OFFSET_M_CUSTOMSCRIPTINT OFFSET_ITEM_ID //[RecvTable.DT_PropSurvival].m_customScriptInt updated 2025/03/31
 #define OFFSET_YAW 0x2294 //[DataMap.C_Player].m_currentFramePlayer.m_ammoPoolCount - 0x8 updated 2025/02/12
  
 //#define OFFSET_GLOW_T1 0x292 //16256 = enabled, 0 = disabled
@@ -105,13 +105,13 @@
 #define GLOW_FADE 0x388+ 0x30 // ats 3rd result of 3 offsets consecutive or first + 8 updated 01/10/2024
 #define HIGHLIGHT_SETTINGS 0x718DFD0 //[Miscellaneous].HighlightSettings updated 2025/02/25
 #define HIGHLIGHT_TYPE_SIZE 0x34 //? updated 01/10/2024
-#define OFFSET_CROSSHAIR_LAST OFFSET_VISIBLE_TIME + 0x19f8 //[Miscellaneous].CWeaponX!lastCrosshairTargetTime updated 2025/03/07
+#define OFFSET_CROSSHAIR_LAST OFFSET_VISIBLE_TIME + 0x19f8 //[Miscellaneous].CWeaponX!lastCrosshairTargetTime updated 2025/03/31
 //#define OFFSET_CROSSHAIR_START 0x1958 //CPlayer!crosshairTargetStartTime updated 01/9/2024
-#define OFFSET_INPUT_SYSTEM 0x1c20e40 //[Miscellaneous].InputSystem updated 2025/03/07 
+#define OFFSET_INPUT_SYSTEM 0x1c63380 //[Miscellaneous].InputSystem updated 2025/03/31 
 
-#define OFFSET_SKYDIVE_STATE 0x4844 //[RecvTable.DT_Player].m_skydiveState updated 2025/03/07
-#define OFFSET_GRAPPLEACTIVED       0x2d90 //[RecvTable.DT_Player].m_grappleActive updated 2025/03/07
-#define OFFSET_GRAPPLE              0x2d08 //[RecvTable.DT_Player].m_grapple updated 2025/03/07
+#define OFFSET_SKYDIVE_STATE 0x4844 //[RecvTable.DT_Player].m_skydiveState updated 2025/03/31
+#define OFFSET_GRAPPLEACTIVED       0x2d90 //[RecvTable.DT_Player].m_grappleActive updated 2025/03/31
+#define OFFSET_GRAPPLE              0x2d08 //[RecvTable.DT_Player].m_grapple updated 2025/03/31
 #define OFFSET_GRAPPLEATTACHED      0x0048 //[RecvTable.DT_GrappleData].m_grappleAttached updated 2025/02/25
-#define OFFSET_m_xp		    0x380c //[RecvTable.DT_Player].m_xp updated 2025/03/07
+#define OFFSET_m_xp		    0x380c //[RecvTable.DT_Player].m_xp updated 2025/03/31
 #define OFFSET_GRADE 0x0348 //[RecvTable.DT_BaseEntity].m_grade updated 2025/02/25
